10f68aa6162d85e90c12247cfb867c50c764b1d6
Daniel <d.t.nguyen@live.com>
2018-10-28 22:16:14 +0000

    used methods

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    
+    // prints out the provided task's details
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    // marks the provided task as completed
+    markCompleted: function (){
+      this.complete = true;
+    }
   };
   return task;
-}
-
-// marks the provided task as completed
-function completeTask(task) {
-  task.complete = true;
-}
-
-// prints out the provided task's details
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
@@ -24,6 +24,6 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
