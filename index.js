c8a4e32222bef4003e3dcf1efb2f1415cbfced7f
Daniel <d.t.nguyen@live.com>
2018-10-28 22:10:49 +0000

    removed commented out code

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// Arrays to keep track of each task's state
-// const taskTitles = [];
-// const taskComplete = [];
-// const taskDescriptions =[];
+// Object to keep track of each task's state
+// // A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
     title: title,
@@ -11,30 +9,10 @@ function newTask(title, description) {
   return task;
 }
 
-// // Create a new task by adding to the arrays
-// // A new task will be created as incomplete
-// function newTask(title) {
-//   taskTitles.push(title);
-//   taskDescriptions.push(description);
-//   taskComplete.push(false);
-// }
-
-// // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-// function completeTask(taskIndex) {
-//   taskComplete[taskIndex] = true;
-// }ß
-
 // marks the provided task as completed
 function completeTask(task) {
   task.complete = true;
 }
-
-// // Print the state of a task to the console in a nice readable way
-// function logTaskState(taskIndex) {
-//   const title = taskTitles[taskIndex];
-//   const complete = taskComplete[taskIndex];
-//   console.log(`${title} has${complete ? " " : " not "}been completed`);
-// }
 
 // prints out the provided task's details
 function logTaskState(task) {
@@ -42,20 +20,10 @@ function logTaskState(task) {
 }
 
 // DRIVER CODE BELOW
-
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-// newTask("Clean Cat Litter"); // task 0
-// newTask("Do Laundry"); // task 1
-
-// logTaskState(0); // Clean Cat Litter has not been completed
-// completeTask(0);
-// logTaskState(0); // Clean Cat Litter has been completed
 logTaskState(task1); // Clean Cat Litter has not been completed
 completeTask(task1);
 logTaskState(task1); // Clean Cat Litter has been completed
-
-// console.log(tasks);
-
